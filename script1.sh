8ca2f01c509ea036ff4ccb94a60eb9f0bd05756f
Mahi Pandey <mahipandey1813@gmail.com>
2026-03-31 14:15:29 +0530

    Update script1.sh

--- script1.sh
+++ script1.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Mahi | Course: Open Source Software
 
+# --- Variables ---
+STUDENT_NAME="Mahi"
+SOFTWARE_CHOICE="Git"
+
+# --- System info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DISTRO=$(lsb_release -d | cut -f2)
+DATE=$(date)
+HOME_DIR=$HOME
+
+# --- Display ---
+echo "================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo "================================"
+echo "Software Chosen : $SOFTWARE_CHOICE"
+echo "Linux Distro    : $DISTRO"
+echo "Kernel Version  : $KERNEL"
+echo "Current User    : $USER_NAME"
+echo "Home Directory  : $HOME_DIR"
+echo "System Uptime   : $UPTIME"
+echo "Current Date    : $DATE"
+echo "License         : GNU GPL v3 (Ubuntu)"
+echo "================================"
